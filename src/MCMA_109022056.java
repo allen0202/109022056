86e53ad03a37c22f65d129321c694e1941ed89bb
allen0202 <nhpss97082@gmailc.o>
2021-02-25 12:30:47 +0800

    Test!!Test!!

--- src/MCMA_109022056.java
+++ src/MCMA_109022056.java
@@ -1,5 +1,5 @@
 public class MCMA_109022056 {
     public static void main(String[] args){
-        System.out.print("My First Java Code!!");
+        System.out.println("My First Java Code!");
     }
 }
